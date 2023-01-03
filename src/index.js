3c277089395f0905b66dbf3cf3e48cd67041415d
lappavel <wizd@tut.by>
2023-01-03 20:14:43 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (n) {
+    let x = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+	let x1 = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+	let xx = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+	let num = n.toString().split('');
+	let result = '';
+	if (n === 0) {return 'zero'};
+	num.length === 1 ? num.unshift('0', '0') : num.length === 2 ? num.unshift('0') : result += `${x[num[0]]} hundred`;
+	result += num[1] == 1 ? ` ${x1[num[2]]}` : ` ${xx[num[1]]}`;
+    result = result.trim();
+    result += num[1] != 1 ? ` ${x[num[2]]}` : '';
+	return result.trim();
 }
